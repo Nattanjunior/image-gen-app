b7b6e383b136b7fd01bf5f45065351ea98ba0cfc
nattanjunior <fnatanieljunior@gmail.com>
2025-05-03 17:17:53 -0300

    Implementa redirecionamento da página inicial para a tela de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View>
+      <Redirect href="/login" />
     </View>
   );
 }
