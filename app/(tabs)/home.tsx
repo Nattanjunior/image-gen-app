68ddf213430dbcf96fc2dbc6edb2137a5e519ebc
nattanjunior <fnatanieljunior@gmail.com>
2025-05-05 02:23:55 -0300

    feat: criar página inicial com listagem de modelos AI

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from 'react-native';
+import AiFeaturedModel from '@/components/home/AiFeaturedModel';
+import AiModels from '@/components/home/aiModels';
+import Banner from '@/components/home/Banner';
+import Header from '@/components/home/Header';
+import { FlatList, View } from 'react-native';
 
 export default function Home() {
- return (
-   <View>
-    <Text>Home</Text>
-   </View>
+  return (
+    <FlatList className='p-5 mt-10'
+      data={[1]}
+      renderItem={(item) => (
+        <View>
+          {/* Header */}
+          <Header />
+          {/* Banner */}
+          <Banner />
+          {/* Featured List */}
+          <AiFeaturedModel />
+
+          {/* Ai Models (Avatar) */}
+          <AiModels type='avatar' />
+
+          {/* Ai Models (Style) */}
+          <AiModels type='style' />
+
+          <View className='h-[100px]'></View>
+        </View>
+      )}
+    >
+
+    </FlatList>
   );
 }
