d59ba7b7e54bd9e4d1a04ba6458895a7c38735bb
nattanjunior <fnatanieljunior@gmail.com>
2025-05-05 04:01:10 -0300

    feat: implementar listagem de modelos AI na página inicial

--- components/home/aiModels.tsx
+++ components/home/aiModels.tsx
@@ -1,9 +1,11 @@
 import GlobalApi from '@/services/GlobalApi';
+import { useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 export default function AiModels({ type }: { type: string }) {
   
   const [aiModelList, setAiModelList] = useState<any[]>([])
+  const router = useRouter()
 
   useEffect(() => {
     GetAiModelList()
@@ -11,10 +13,12 @@ export default function AiModels({ type }: { type: string }) {
 
   const GetAiModelList = async () => {
     const response = await GlobalApi.GetAiModelList(type)
-    console.log(response)
     setAiModelList(response.data.data)
   }
 
+  const onPressAiModel = () => {
+    router.push({pathname: '/FormInput',})
+  }
 
   return (
     <View>
@@ -25,12 +29,12 @@ export default function AiModels({ type }: { type: string }) {
         showsHorizontalScrollIndicator={false}
         nestedScrollEnabled={true}
         renderItem={({ item }) => (
-          <View className='mr-4'>
+          <TouchableOpacity className='mr-4' onPress={() => onPressAiModel()}>
             <Image source={{ uri: item?.banner?.url }}
               className='w-[140px] h-[140px] rounded-2xl'
             />
             <Text className='absolute bottom-2 left-2 text-white text-lg font-medium'>{item?.name}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>
